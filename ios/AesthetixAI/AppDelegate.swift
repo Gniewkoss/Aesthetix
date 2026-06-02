cd2dc6a0693f39de47391e8af9e623fcb8474dc5
Gniewko Koscielak <gniewkoscielak@gmail.com>
2026-06-02 18:43:24 +0200

    refactor: update bundle URL configuration for debug builds
    - Changed the preprocessor directive for the bundle URL in AppDelegate.swift to use EXPO_CONFIGURATION_DEBUG for improved clarity in debug environments.
    - This adjustment enhances the build configuration management for React Native applications.

--- ios/AesthetixAI/AppDelegate.swift
+++ ios/AesthetixAI/AppDelegate.swift
@@ -61,7 +61,7 @@ class ReactNativeDelegate: ExpoReactNativeFactoryDelegate {
   }
 
   override func bundleURL() -> URL? {
-#if DEBUG
+#if EXPO_CONFIGURATION_DEBUG
     return RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: ".expo/.virtual-metro-entry")
 #else
     return Bundle.main.url(forResource: "main", withExtension: "jsbundle")
